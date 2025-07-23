92d5967c71805f97da765ff5d6cc1e07bae47332
Bharatgithu <bharatkumarraghav4@gmail.com>
2025-07-23 20:34:54 +0530

    final 2

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://mcqgenerater.netlify.app'
+}));
+
 app.use(express.json());
 
 app.post("/generate-mcqs", async (req, res) => {
